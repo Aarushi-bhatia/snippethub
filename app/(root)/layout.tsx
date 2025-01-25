af7e60a45b2c9bc8f62e5b889db00bcc751e1c70
Aarushi Bhatia <aarushibhatia073@gmail.com>
2025-01-25 10:50:56 +0530

    changes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,9 @@
 import Navbar from "../components/Navbar";
-import '../../styles/globals.css';
+//import '../../styles/globals.css';
 
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>){
   return (
-    <main className="app">
+    <main className="">
       <Navbar />
       {children}
       </main>
